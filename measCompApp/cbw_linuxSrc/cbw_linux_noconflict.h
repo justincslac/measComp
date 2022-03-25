0e77cb749c2ebfa333714f06bc225b7a90ad169b
Mark Rivers <rivers@cars.uchicago.edu>
2022-03-25 18:23:23 -0500

    Thin wrapper on cbw_noconflict.h

---
+++ measCompApp/cbw_linuxSrc/cbw_linux_noconflict.h
@@ -0,0 +1,20 @@
+#ifndef cbwLinuxInclude
+#define cbwLinuxInclude
+
+#include <stdlib.h>
+#include <string.h>
+
+typedef char CHAR;
+typedef unsigned char BYTE;
+typedef unsigned short USHORT;
+typedef unsigned int UINT;
+typedef unsigned long long ULONGLONG;
+typedef int INT;
+typedef int LONG;
+typedef unsigned int ULONG;
+typedef void * HGLOBAL;
+#define __stdcall
+
+#include "cbw_noconflict.h"
+
+#endif
